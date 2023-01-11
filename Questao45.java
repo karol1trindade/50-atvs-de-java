aee1e10898503a6ee363f15b72c0246754902b16
Karol linda <121971143+karol1trindade@users.noreply.github.com>
2023-01-11 01:07:50 -0300

    Add files via upload

---
+++ Questao45.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class Questao45 {
+    /* Elabore um programa que leia o nome e a idade de cinco pessoas e apresente:
+a maior idade, nome da pessoa mais nova, média das idades.
+ */
+}
